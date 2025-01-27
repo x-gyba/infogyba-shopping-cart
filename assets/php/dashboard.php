b5957d6f3bc19a4fbd6ac3ab364f398f80ead27f
Clayton Campos <30420230+x-gyba@users.noreply.github.com>
2025-01-27 13:47:42 -0300

    Delete assets directory

--- assets/php/dashboard.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Gerar a senha criptografada
-$senha = 'Elshadday2030!';
-$senhaCriptografada = password_hash($senha, PASSWORD_DEFAULT);
-
-// Exibir a senha criptografada
-echo $senhaCriptografada;
-?>
--- assets/php/payment.php
+++
@@ -1,6 +0,0 @@
-<!-- content from payment.php -->
-<div class="payment-container">
-    <h2>Pagamento</h2>
-    <p>Informações de pagamento aqui.</p>
-    <!-- Formulário de pagamento aqui -->
-</div>
